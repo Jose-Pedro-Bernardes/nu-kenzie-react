d7ea3b6d21e3c3b217ca3267cfd31627c0e2dde2
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-02-03 23:58:21 -0300

    add children button

--- nu-kenzie/src/components/HeaderContainer/index.jsx
+++ nu-kenzie/src/components/HeaderContainer/index.jsx
@@ -9,11 +9,7 @@ export default function HeaderContainer({ children }) {
       <header className={styles.header__container}>
         <div className={styles.header__align}>
           <img src={NuKenzieDash} alt="Nu Kenzie Logo" />
-          <MainButton
-            onClick={props}
-            newClass={styles.header__button}
-            value="Início"
-          />
+          {children}
         </div>
       </header>
     </>
